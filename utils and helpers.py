069f95082061df1770e86fcd9a74c9331f01d017
jishnumr-ghub <jishnumr02@gmail.com>
2025-06-26 14:31:40 +0530

    Create utils and helpers.py

---
+++ utils and helpers.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+import random
+
+def set_seed(seed=42):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+def flatten_preds(preds):
+    return preds.view(-1).cpu().numpy()
