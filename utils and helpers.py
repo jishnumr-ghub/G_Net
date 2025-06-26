a2ae67e380d808593874e77712564f3e8055bdeb
jishnumr-ghub <jishnumr02@gmail.com>
2025-06-26 14:32:07 +0530

    utils and helpers.py

--- utils and helpers.py
+++ utils and helpers.py
@@ -8,6 +8,6 @@ def set_seed(seed=42):
     random.seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-
+ 
 def flatten_preds(preds):
     return preds.view(-1).cpu().numpy()
